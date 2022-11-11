ce172ed292821855fdda77d7d025c68f8ce75579
kiri <983243500@qq.com>
2022-11-11 20:06:05 +0800

    first git

---
+++ src/com/ziri/gittest/testhello.java
@@ -0,0 +1,11 @@
+package com.ziri.gittest;
+
+/**
+ * @author 况志勤
+ * @version 1.0
+ */
+public class testhello {
+    public static void main(String[] args) {
+        System.out.printf("hello git");
+    }
+}
